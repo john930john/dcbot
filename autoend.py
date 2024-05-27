9998ab021017730a93cf13fb23a1daa9b0423d33
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-27 22:21:20 +0400

    Update autoend.py

--- autoend.py
+++ autoend.py
@@ -3,22 +3,23 @@ from pyrogram.types import Message
 
 from AnonXMusic import app
 from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import autoend_off, autoend_on
+from AnonXMusic.utils.database import otomatik_sonlandırma_kapat, otomatik_sonlandırma_ac
 
 
-@app.on_message(filters.command("autoend") & SUDOERS)
-async def auto_end_stream(_, message: Message):
-    usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
+@app.on_message(filters.command("otomatiksonlandırma") & SUDOERS)
+async def otomatik_sonlandırma_ak_kapat(_, message: Message):
+    kullanim = "<b>Örnek:</b>\n\n/otomatiksonlandırma [aç | kapat]"
     if len(message.command) != 2:
-        return await message.reply_text(usage)
-    state = message.text.split(None, 1)[1].strip().lower()
-    if state == "enable":
-        await autoend_on()
+        return await message.reply_text(kullanim)
+    durum = message.text.split(None, 1)[1].strip().lower()
+    if durum == "aç":
+        await otomatik_sonlandırma_ac()
         await message.reply_text(
-            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
+            "» Otomatik sonlandırma özelliği açıldı.\n\nAsistan, kimse dinlemediğinde video sohbeti otomatik olarak terk edecektir."
         )
-    elif state == "disable":
-        await autoend_off()
-        await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
+    elif durum == "kapat":
+        await otomatik_sonlandırma_kapat()
+        await message.reply_text("» Otomatik sonlandırma özelliği kapatıldı.")
     else:
-        await message.reply_text(usage)
+        await message.reply_text(kullanim)
+        
