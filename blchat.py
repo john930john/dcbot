892563d656de3d3047ea197f028af2801e39dd23
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-27 22:31:24 +0400

    Update blchat.py

--- blchat.py
+++ blchat.py
@@ -7,55 +7,53 @@ from AnonXMusic.utils.database import blacklist_chat, blacklisted_chats, whiteli
 from AnonXMusic.utils.decorators.language import language
 from config import BANNED_USERS
 
-
-@app.on_message(filters.command(["blchat", "blacklistchat"]) & SUDOERS)
+@app.on_message(filters.command(["siyahliste", "sohbetekle"]) & SUDOERS)
 @language
-async def blacklist_chat_func(client, message: Message, _):
+async def siyah_liste_ekle(client, message: Message, _):
     if len(message.command) != 2:
-        return await message.reply_text(_["black_1"])
-    chat_id = int(message.text.strip().split()[1])
-    if chat_id in await blacklisted_chats():
-        return await message.reply_text(_["black_2"])
-    blacklisted = await blacklist_chat(chat_id)
-    if blacklisted:
-        await message.reply_text(_["black_3"])
+        return await message.reply_text(_["siyah_1"])
+    sohbet_id = int(message.text.strip().split()[1])
+    if sohbet_id in await blacklisted_chats():
+        return await message.reply_text(_["siyah_2"])
+    siyah_listeye_eklendi = await blacklist_chat(sohbet_id)
+    if siyah_listeye_eklendi:
+        await message.reply_text(_["siyah_3"])
     else:
-        await message.reply_text(_["black_9"])
+        await message.reply_text(_["siyah_9"])
     try:
-        await app.leave_chat(chat_id)
+        await app.leave_chat(sohbet_id)
     except:
         pass
 
-
 @app.on_message(
-    filters.command(["whitelistchat", "unblacklistchat", "unblchat"]) & SUDOERS
+    filters.command(["beyazliste", "siyahlistedengelcıkar", "siyahlistedencıkar"]) & SUDOERS
 )
 @language
-async def white_funciton(client, message: Message, _):
+async def beyaz_liste(client, message: Message, _):
     if len(message.command) != 2:
-        return await message.reply_text(_["black_4"])
-    chat_id = int(message.text.strip().split()[1])
-    if chat_id not in await blacklisted_chats():
-        return await message.reply_text(_["black_5"])
-    whitelisted = await whitelist_chat(chat_id)
-    if whitelisted:
-        return await message.reply_text(_["black_6"])
-    await message.reply_text(_["black_9"])
+        return await message.reply_text(_["siyah_4"])
+    sohbet_id = int(message.text.strip().split()[1])
+    if sohbet_id not in await blacklisted_chats():
+        return await message.reply_text(_["siyah_5"])
+    beyaz_listeye_eklendi = await whitelist_chat(sohbet_id)
+    if beyaz_listeye_eklendi:
+        return await message.reply_text(_["siyah_6"])
+    await message.reply_text(_["siyah_9"])
 
-
-@app.on_message(filters.command(["blchats", "blacklistedchats"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["siyahlisteler", "siyahlistedensohbetler"]) & ~BANNED_USERS)
 @language
-async def all_chats(client, message: Message, _):
-    text = _["black_7"]
+async def tum_sohbetler(client, message: Message, _):
+    text = _["siyah_7"]
     j = 0
-    for count, chat_id in enumerate(await blacklisted_chats(), 1):
+    for say, sohbet_id in enumerate(await blacklisted_chats(), 1):
         try:
-            title = (await app.get_chat(chat_id)).title
+            baslik = (await app.get_chat(sohbet_id)).title
         except:
-            title = "ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+            baslik = "Özel Sohbet"
         j = 1
-        text += f"{count}. {title}[<code>{chat_id}</code>]\n"
+        text += f"{say}. {baslik}[<code>{sohbet_id}</code>]\n"
     if j == 0:
-        await message.reply_text(_["black_8"].format(app.mention))
+        await message.reply_text(_["siyah_8"].format(app.mention))
     else:
         await message.reply_text(text)
+        
