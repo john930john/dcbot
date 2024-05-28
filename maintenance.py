7085e6e604806778066093389d38d1fc952b4cc6
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-28 23:52:19 +0400

    Update maintenance.py

--- maintenance.py
+++ maintenance.py
@@ -12,28 +12,28 @@ from AnonXMusic.utils.database import (
 from strings import get_string
 
 
-@app.on_message(filters.command(["maintenance"]) & SUDOERS)
-async def maintenance(client, message: Message):
+@app.on_message(filters.command(["bakım"]) & SUDOERS)
+async def bakim(client, message: Message):
     try:
-        language = await get_lang(message.chat.id)
-        _ = get_string(language)
+        dil = await get_lang(message.chat.id)
+        _ = get_string(dil)
     except:
-        _ = get_string("en")
-    usage = _["maint_1"]
+        _ = get_string("tr")  # Varsayılan olarak Türkçe dil kullanılacak
+    kullanım = _["maint_1"]
     if len(message.command) != 2:
-        return await message.reply_text(usage)
-    state = message.text.split(None, 1)[1].strip().lower()
-    if state == "enable":
+        return await message.reply_text(kullanım)
+    durum = message.text.split(None, 1)[1].strip().lower()
+    if durum == "etkinleştir":
         if await is_maintenance() is False:
             await message.reply_text(_["maint_4"])
         else:
             await maintenance_on()
             await message.reply_text(_["maint_2"].format(app.mention))
-    elif state == "disable":
+    elif durum == "devre dışı":
         if await is_maintenance() is False:
             await maintenance_off()
             await message.reply_text(_["maint_3"].format(app.mention))
         else:
             await message.reply_text(_["maint_5"])
     else:
-        await message.reply_text(usage)
+        await message.reply_text(kullanım)
