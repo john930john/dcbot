414587600132e1dcd6e6a0b8d58b7bf9565d39f5
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-28 19:23:28 +0400

    Update dir.py

--- dir.py
+++ dir.py
@@ -1,20 +1,21 @@
 import os
 
-from ..logging import LOGGER
+from ..günlükleme import GÜNCELLEYİCİ
 
 
-def dirr():
-    for file in os.listdir():
-        if file.endswith(".jpg"):
-            os.remove(file)
-        elif file.endswith(".jpeg"):
-            os.remove(file)
-        elif file.endswith(".png"):
-            os.remove(file)
+def dizinleri_güncelle():
+    for dosya in os.listdir():
+        if dosya.endswith(".jpg"):
+            os.remove(dosya)
+        elif dosya.endswith(".jpeg"):
+            os.remove(dosya)
+        elif dosya.endswith(".png"):
+            os.remove(dosya)
 
-    if "downloads" not in os.listdir():
-        os.mkdir("downloads")
-    if "cache" not in os.listdir():
-        os.mkdir("cache")
+    if "indirilenler" not in os.listdir():
+        os.mkdir("indirilenler")
+    if "önbellek" not in os.listdir():
+        os.mkdir("önbellek")
 
-    LOGGER(__name__).info("Directories Updated.")
+    GÜNCELLEYİCİ(__name__).bilgi("Dizinler Güncellendi.")
+    
