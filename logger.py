a6ae81f8657b736d7917a733af4d417e41127e1e
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-28 20:00:11 +0400

    Update logger.py

--- logger.py
+++ logger.py
@@ -4,30 +4,30 @@ from AnonXMusic import app
 from AnonXMusic.utils.database import is_on_off
 from config import LOGGER_ID
 
-
-async def play_logs(message, streamtype):
+async def oynatma_kayitlari(mesaj, akis_turu):
     if await is_on_off(2):
-        logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+        kayitci_metin = f"""
+<b>{app.mention} OYNATMA KAYDI</b>
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+<b>Sohbet ID :</b> <code>{mesaj.chat.id}</code>
+<b>Sohbet Adı :</b> {mesaj.chat.title}
+<b>Sohbet Kullanıcı Adı :</b> @{mesaj.chat.username}
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+<b>Kullanıcı ID :</b> <code>{mesaj.from_user.id}</code>
+<b>Adı :</b> {mesaj.from_user.mention}
+<b>Kullanıcı Adı :</b> @{mesaj.from_user.username}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
-        if message.chat.id != LOGGER_ID:
+<b>Sorgu :</b> {mesaj.text.split(None, 1)[1]}
+<b>Akış Türü :</b> {akis_turu}"""
+        if mesaj.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
                     chat_id=LOGGER_ID,
-                    text=logger_text,
+                    text=kayitci_metin,
                     parse_mode=ParseMode.HTML,
                     disable_web_page_preview=True,
                 )
             except:
                 pass
         return
+        
