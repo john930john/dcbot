ff7266f5dea725b8e94a278b0c6aec323d761185
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-28 19:36:19 +0400

    Update help.py

--- help.py
+++ help.py
@@ -5,114 +5,115 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from AnonXMusic import app
 
 
-def help_pannel(_, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
-    second = [
+def yardım_paneli(_, BAŞLANGIÇ: Union[bool, int] = None):
+    ilk = [InlineKeyboardButton(text=_["KAPAT_BUTTON"], callback_data=f"kapat")]
+    ikinci = [
         InlineKeyboardButton(
-            text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
+            text=_["GERİ_BUTTON"],
+            callback_data=f"ayarlar_geri_yardımcı",
         ),
     ]
-    mark = second if START else first
-    upl = InlineKeyboardMarkup(
+    işaret = ikinci if BAŞLANGIÇ else ilk
+    düzen = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
-                    text=_["H_B_1"],
-                    callback_data="help_callback hb1",
+                    text=_["Y_B_1"],
+                    callback_data="yardım_callback yb1",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_2"],
-                    callback_data="help_callback hb2",
+                    text=_["Y_B_2"],
+                    callback_data="yardım_callback yb2",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_3"],
-                    callback_data="help_callback hb3",
+                    text=_["Y_B_3"],
+                    callback_data="yardım_callback yb3",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
+                    text=_["Y_B_4"],
+                    callback_data="yardım_callback yb4",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
+                    text=_["Y_B_5"],
+                    callback_data="yardım_callback yb5",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb6",
+                    text=_["Y_B_6"],
+                    callback_data="yardım_callback yb6",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
+                    text=_["Y_B_7"],
+                    callback_data="yardım_callback yb7",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_8"],
-                    callback_data="help_callback hb8",
+                    text=_["Y_B_8"],
+                    callback_data="yardım_callback yb8",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_9"],
-                    callback_data="help_callback hb9",
+                    text=_["Y_B_9"],
+                    callback_data="yardım_callback yb9",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text=_["H_B_10"],
-                    callback_data="help_callback hb10",
+                    text=_["Y_B_10"],
+                    callback_data="yardım_callback yb10",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_11"],
-                    callback_data="help_callback hb11",
+                    text=_["Y_B_11"],
+                    callback_data="yardım_callback yb11",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_12"],
-                    callback_data="help_callback hb12",
+                    text=_["Y_B_12"],
+                    callback_data="yardım_callback yb12",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text=_["H_B_13"],
-                    callback_data="help_callback hb13",
+                    text=_["Y_B_13"],
+                    callback_data="yardım_callback yb13",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_14"],
-                    callback_data="help_callback hb14",
+                    text=_["Y_B_14"],
+                    callback_data="yardım_callback yb14",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_15"],
-                    callback_data="help_callback hb15",
+                    text=_["Y_B_15"],
+                    callback_data="yardım_callback yb15",
                 ),
             ],
-            mark,
+            işaret,
         ]
     )
-    return upl
+    return düzen
 
 
-def help_back_markup(_):
-    upl = InlineKeyboardMarkup(
+def yardım_geri_düzen(_):
+    düzen = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
-                    text=_["BACK_BUTTON"],
-                    callback_data=f"settings_back_helper",
+                    text=_["GERİ_BUTTON"],
+                    callback_data=f"ayarlar_geri_yardımcı",
                 ),
             ]
         ]
     )
-    return upl
+    return düzen
 
 
-def private_help_panel(_):
-    buttons = [
+def özel_yardım_paneli(_):
+    düğmeler = [
         [
             InlineKeyboardButton(
-                text=_["S_B_4"],
-                url=f"https://t.me/{app.username}?start=help",
+                text=_["Ö_B_4"],
+                url=f"https://t.me/{app.username}?start=yardım",
             ),
         ],
     ]
-    return buttons
+    return düğmeler
+    
