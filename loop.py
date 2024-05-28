156566635ff85e8aad463d7b84f830a04fe70505
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-28 23:48:12 +0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -11,36 +11,37 @@ from config import BANNED_USERS
 @app.on_message(filters.command(["loop", "cloop", "tekrarla"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def admins(cli, message: Message, _, chat_id):
-    usage = _["admin_17"]
+    kullanım = _["admin_17"]
     if len(message.command) != 2:
-        return await message.reply_text(usage)
-    state = message.text.split(None, 1)[1].strip()
-    if state.isnumeric():
-        state = int(state)
-        if 1 <= state <= 10:
-            got = await get_loop(chat_id)
-            if got != 0:
-                state = got + state
-            if int(state) > 10:
-                state = 10
-            await set_loop(chat_id, state)
+        return await message.reply_text(kullanım)
+    durum = message.text.split(None, 1)[1].strip()
+    if durum.isnumeric():
+        durum = int(durum)
+        if 1 <= durum <= 10:
+            var = await get_loop(chat_id)
+            if var != 0:
+                durum = var + durum
+            if int(durum) > 10:
+                durum = 10
+            await set_loop(chat_id, durum)
             return await message.reply_text(
-                text=_["admin_18"].format(state, message.from_user.mention),
+                text=_["admin_18"].format(durum, message.from_user.mention),
                 reply_markup=close_markup(_),
             )
         else:
             return await message.reply_text(_["admin_17"])
-    elif state.lower() == "enable":
+    elif durum.lower() == "etkinleştir":
         await set_loop(chat_id, 10)
         return await message.reply_text(
-            text=_["admin_18"].format(state, message.from_user.mention),
+            text=_["admin_18"].format(durum, message.from_user.mention),
             reply_markup=close_markup(_),
         )
-    elif state.lower() == "disable":
+    elif durum.lower() == "devre dışı":
         await set_loop(chat_id, 0)
         return await message.reply_text(
             _["admin_19"].format(message.from_user.mention),
             reply_markup=close_markup(_),
         )
     else:
-        return await message.reply_text(usage)
+        return await message.reply_text(kullanım)
+        
