8cb3673df49cd2e23dbab42c8039784423d3fd8d
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-28 18:54:34 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,89 +6,61 @@ from pyrogram import filters
 
 load_dotenv()
 
-# Get this value from my.telegram.org/apps
+# Bu değeri my.telegram.org/apps adresinden alın.
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
-# Get your token from @BotFather on Telegram.
+# Botunuzun kimlik belirtecisini @BotFather üzerinden alın.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-# Get your mongo url from cloud.mongodb.com
+# MongoDB bağlantı URI'sini cloud.mongodb.com adresinden alın.
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 360))
 
-SONG_DOWNLOAD_DURATION = int(
-    getenv("SONG_DOWNLOAD_DURATION_LIMIT", "5400")
-)
-# Chat id of a group for logging bot's activities
+SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "5400"))
+
+# Botun etkinliklerini kaydetmek için bir grup sohbetinin kimliği.
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
-# Get this value from @FallenxBot on Telegram by /id
+# Bu değeri @FallenxBot üzerinden /id komutuyla alın.
 OWNER_ID = int(getenv("OWNER_ID", 6532412571))
 
-## Fill these variables if you're deploying on heroku.
-# Your heroku app name
+# Heroku'da dağıtıyorsanız bu değişkenleri doldurun.
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-# Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/mamakli06/mamaklimusic1",
-)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/mamakli06/mamaklimusic1")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)  # Fill this variable if your upstream repository is private
+GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.mamaklibirininruhu")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mamaklimekani")
 
-# Set this to True if you want the assistant to automatically leave chats after an interval
+# Asistanın otomatik olarak sohbetlerden ayrılmasını istiyorsanız True olarak ayarlayın.
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 
-
-# Get this credentials from https://developer.spotify.com/dashboard
+# Spotify'dan API anahtarlarını https://developer.spotify.com/dashboard adresinden alın.
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
-
-# Maximum limit for fetching playlist's track from youtube, spotify, apple links.
+# Youtube, Spotify ve Apple müzik bağlantılarından alınacak maksimum çalma listesi sayısı.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 
-
-# Telegram audio and video file size limit (in bytes)
+# Telegram ses ve video dosya boyut limiti (byte cinsinden)
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 314572800))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 3221225472))
-# Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
-
-# Get your pyrogram v2 session from @StringFatherBot on Telegram
+# Pyrogram v2 oturumları için buradan @StringFatherBot üzerinden alın.
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
 
-
-BANNED_USERS = filters.user()
-adminlist = {}
-lyrical = {}
-votemode = {}
-chatstats = {}
-userstats = {}
-clean = {}
-autoclean = []
-confirmer = {}
-
-
-START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/Yeni-11-29"
-)
-PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/Yeni-11-29"
-)
+# Bazı URL'ler
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/Yeni-11-29")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://telegra.ph/Yeni-11-29")
 PLAYLIST_IMG_URL = "https://telegra.ph/Eski-11-29"
 STATS_IMG_URL = "https://telegra.ph/Eski-11-29"
 TELEGRAM_AUDIO_URL = "https://telegra.ph/Eski-11-29"
@@ -101,25 +73,24 @@ SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/Eski-11-29"
 SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/Eski-11-29"
 
 
-def time_to_seconds(time):
-    stringt = str(time)
-    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+def zaman_dakika_cevir(sure):
+    sure_str = str(sure)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(sure_str.split(":"))))
 
 
-DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
-SONG_DOWNLOAD_DURATION_LIMIT = int(
-    time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00"))
-
+DURATION_LIMIT = int(zaman_dakika_cevir(f"{DURATION_LIMIT_MIN}:00"))
+SONG_DOWNLOAD_DURATION_LIMIT = int(zaman_dakika_cevir(f"{SONG_DOWNLOAD_DURATION}:00"))
 
 
 if SUPPORT_CHANNEL:
     if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
         raise SystemExit(
-            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+            "[HATA] - SUPPORT_CHANNEL URL'niz yanlış. Lütfen https:// ile başladığından emin olun."
         )
 
 if SUPPORT_CHAT:
     if not re.match("(?:http|https)://", SUPPORT_CHAT):
         raise SystemExit(
-            "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
+            "[HATA] - SUPPORT_CHAT URL'niz yanlış. Lütfen https:// ile başladığından emin olun."
         )
+        
