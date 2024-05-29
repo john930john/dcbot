3c2cba055b0d47a9fa83c1266c83d091034a585b
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-29 21:20:02 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,110 +3,111 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton
 
 
-def setting_markup(_):
+def ayar_markup(dil):
     buttons = [
         [
-            InlineKeyboardButton(text=_["ST_B_1"], callback_data="AU"),
-            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+            InlineKeyboardButton(text=dil["ST_B_1"], callback_data="AU"),
+            InlineKeyboardButton(text=dil["ST_B_3"], callback_data="LG"),
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_2"], callback_data="PM"),
+            InlineKeyboardButton(text=dil["ST_B_2"], callback_data="PM"),
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_4"], callback_data="VM"),
+            InlineKeyboardButton(text=dil["ST_B_4"], callback_data="VM"),
         ],
         [
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text=dil["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
 
 
-def vote_mode_markup(_, current, mode: Union[bool, str] = None):
+def oy_modu_ayarları(dil, şuanki, mod: Union[bool, str] = None):
     buttons = [
         [
-            InlineKeyboardButton(text="Vᴏᴛɪɴɢ ᴍᴏᴅᴇ ➜", callback_data="VOTEANSWER"),
+            InlineKeyboardButton(text="Oy Modu ➜", callback_data="VOTEANSWER"),
             InlineKeyboardButton(
-                text=_["ST_B_5"] if mode == True else _["ST_B_6"],
+                text=dil["ST_B_5"] if mod == True else dil["ST_B_6"],
                 callback_data="VOMODECHANGE",
             ),
         ],
         [
             InlineKeyboardButton(text="-2", callback_data="FERRARIUDTI M"),
             InlineKeyboardButton(
-                text=f"ᴄᴜʀʀᴇɴᴛ : {current}",
+                text=f"şuanki : {şuanki}",
                 callback_data="ANSWERVOMODE",
             ),
             InlineKeyboardButton(text="+2", callback_data="FERRARIUDTI A"),
         ],
         [
             InlineKeyboardButton(
-                text=_["BACK_BUTTON"],
+                text=dil["BACK_BUTTON"],
                 callback_data="settings_helper",
             ),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text=dil["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
 
 
-def auth_users_markup(_, status: Union[bool, str] = None):
+def yetkililer_ayarları(dil, durum: Union[bool, str] = None):
     buttons = [
         [
-            InlineKeyboardButton(text=_["ST_B_7"], callback_data="AUTHANSWER"),
+            InlineKeyboardButton(text=dil["ST_B_7"], callback_data="AUTHANSWER"),
             InlineKeyboardButton(
-                text=_["ST_B_8"] if status == True else _["ST_B_9"],
+                text=dil["ST_B_8"] if durum == True else dil["ST_B_9"],
                 callback_data="AUTH",
             ),
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_1"], callback_data="AUTHLIST"),
+            InlineKeyboardButton(text=dil["ST_B_1"], callback_data="AUTHLIST"),
         ],
         [
             InlineKeyboardButton(
-                text=_["BACK_BUTTON"],
+                text=dil["BACK_BUTTON"],
                 callback_data="settings_helper",
             ),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text=dil["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
 
 
-def playmode_users_markup(
-    _,
-    Direct: Union[bool, str] = None,
-    Group: Union[bool, str] = None,
-    Playtype: Union[bool, str] = None,
+def çalma_modu_ayarları(
+    dil,
+    Direkt: Union[bool, str] = None,
+    Grup: Union[bool, str] = None,
+    Oynatma_tipi: Union[bool, str] = None,
 ):
     buttons = [
         [
-            InlineKeyboardButton(text=_["ST_B_10"], callback_data="SEARCHANSWER"),
+            InlineKeyboardButton(text=dil["ST_B_10"], callback_data="SEARCHANSWER"),
             InlineKeyboardButton(
-                text=_["ST_B_11"] if Direct == True else _["ST_B_12"],
+                text=dil["ST_B_11"] if Direkt == True else dil["ST_B_12"],
                 callback_data="MODECHANGE",
             ),
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_13"], callback_data="AUTHANSWER"),
+            InlineKeyboardButton(text=dil["ST_B_13"], callback_data="AUTHANSWER"),
             InlineKeyboardButton(
-                text=_["ST_B_8"] if Group == True else _["ST_B_9"],
+                text=dil["ST_B_8"] if Grup == True else dil["ST_B_9"],
                 callback_data="CHANNELMODECHANGE",
             ),
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_14"], callback_data="PLAYTYPEANSWER"),
+            InlineKeyboardButton(text=dil["ST_B_14"], callback_data="PLAYTYPEANSWER"),
             InlineKeyboardButton(
-                text=_["ST_B_8"] if Playtype == True else _["ST_B_9"],
+                text=dil["ST_B_8"] if Oynatma_tipi == True else dil["ST_B_9"],
                 callback_data="PLAYTYPECHANGE",
             ),
         ],
         [
             InlineKeyboardButton(
-                text=_["BACK_BUTTON"],
+                text=dil["BACK_BUTTON"],
                 callback_data="settings_helper",
             ),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text=dil["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
+    
