218820c3d08e38fa7aa5758329b7d34cde6b478f
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-29 16:56:12 +0400

    Update play.py

--- play.py
+++ play.py
@@ -5,152 +5,153 @@ from pyrogram.types import InlineKeyboardButton
 from AnonXMusic.utils.formatters import time_to_seconds
 
 
-def track_markup(_, videoid, user_id, channel, fplay):
-    buttons = [
+def takip_düzeni(_, videoid, kullanıcı_id, kanal, fplay):
+    düğmeler = [
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+                callback_data=f"MusicStream {videoid}|{kullanıcı_id}|a|{kanal}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+                callback_data=f"MusicStream {videoid}|{kullanıcı_id}|v|{kanal}|{fplay}",
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
+                callback_data=f"forceclose {videoid}|{kullanıcı_id}",
             )
         ],
     ]
-    return buttons
+    return düğmeler
 
 
-def stream_markup_timer(_, chat_id, played, dur):
-    played_sec = time_to_seconds(played)
-    duration_sec = time_to_seconds(dur)
-    percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "◉—————————"
-    elif 10 < umm < 20:
-        bar = "—◉————————"
-    elif 20 <= umm < 30:
-        bar = "——◉———————"
-    elif 30 <= umm < 40:
-        bar = "———◉——————"
-    elif 40 <= umm < 50:
-        bar = "————◉—————"
-    elif 50 <= umm < 60:
-        bar = "—————◉————"
-    elif 60 <= umm < 70:
-        bar = "——————◉———"
-    elif 70 <= umm < 80:
-        bar = "———————◉——"
-    elif 80 <= umm < 95:
-        bar = "————————◉—"
+def akış_düzeni_zamanlayıcı(_, sohbet_id, oynatılan, dur):
+    oynatılan_saniye = time_to_seconds(oynatılan)
+    süre_saniye = time_to_seconds(dur)
+    yüzde = (oynatılan_saniye / süre_saniye) * 100
+    yuvarlanmış_yüzde = math.floor(yüzde)
+    if 0 < yuvarlanmış_yüzde <= 10:
+        çubuk = "◉—————————"
+    elif 10 < yuvarlanmış_yüzde < 20:
+        çubuk = "—◉————————"
+    elif 20 <= yuvarlanmış_yüzde < 30:
+        çubuk = "——◉———————"
+    elif 30 <= yuvarlanmış_yüzde < 40:
+        çubuk = "———◉——————"
+    elif 40 <= yuvarlanmış_yüzde < 50:
+        çubuk = "————◉—————"
+    elif 50 <= yuvarlanmış_yüzde < 60:
+        çubuk = "—————◉————"
+    elif 60 <= yuvarlanmış_yüzde < 70:
+        çubuk = "——————◉———"
+    elif 70 <= yuvarlanmış_yüzde < 80:
+        çubuk = "———————◉——"
+    elif 80 <= yuvarlanmış_yüzde < 95:
+        çubuk = "————————◉—"
     else:
-        bar = "—————————◉"
-    buttons = [
+        çubuk = "—————————◉"
+    düğmeler = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{sohbet_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{sohbet_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{sohbet_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{sohbet_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{sohbet_id}"),
         ],
         [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
+                text=f"{oynatılan} {çubuk} {dur}",
                 callback_data="GetTimer",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
-    return buttons
+    return düğmeler
 
 
-def stream_markup(_, chat_id):
-    buttons = [
+def akış_düzeni(_, sohbet_id):
+    düğmeler = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{sohbet_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{sohbet_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{sohbet_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{sohbet_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{sohbet_id}"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
-    return buttons
+    return düğmeler
 
 
-def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
-    buttons = [
+def çalma_listesi_düzeni(_, videoid, kullanıcı_id, ptype, kanal, fplay):
+    düğmeler = [
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"AnonyPlaylists {videoid}|{kullanıcı_id}|{ptype}|a|{kanal}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"AnonyPlaylists {videoid}|{kullanıcı_id}|{ptype}|v|{kanal}|{fplay}",
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
+                callback_data=f"forceclose {videoid}|{kullanıcı_id}",
             ),
         ],
     ]
-    return buttons
+    return düğmeler
 
 
-def livestream_markup(_, videoid, user_id, mode, channel, fplay):
-    buttons = [
+def canlı_akış_düzeni(_, videoid, kullanıcı_id, mod, kanal, fplay):
+    düğmeler = [
         [
             InlineKeyboardButton(
                 text=_["P_B_3"],
-                callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
+                callback_data=f"LiveStream {videoid}|{kullanıcı_id}|{mod}|{kanal}|{fplay}",
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {videoid}|{user_id}",
+                callback_data=f"forceclose {videoid}|{kullanıcı_id}",
             ),
         ],
     ]
-    return buttons
+    return düğmeler
 
 
-def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
-    query = f"{query[:20]}"
-    buttons = [
+def kaydırıcı_düzeni(_, videoid, kullanıcı_id, sorgu, sorgu_tipi, kanal, fplay):
+    sorgu = f"{sorgu[:20]}"
+    düğmeler = [
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+                callback_data=f"MusicStream {videoid}|{kullanıcı_id}|a|{kanal}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+                callback_data=f"MusicStream {videoid}|{kullanıcı_id}|v|{kanal}|{fplay}",
             ),
         ],
         [
             InlineKeyboardButton(
                 text="◁",
-                callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+                callback_data=f"slider B|{sorgu_tipi}|{sorgu}|{kullanıcı_id}|{kanal}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
-                callback_data=f"forceclose {query}|{user_id}",
+                callback_data=f"forceclose {sorgu}|{kullanıcı_id}",
             ),
             InlineKeyboardButton(
                 text="▷",
-                callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+                callback_data=f"slider F|{sorgu_tipi}|{sorgu}|{kullanıcı_id}|{kanal}|{fplay}",
             ),
         ],
     ]
-    return buttons
+    return düğmeler
+                      
