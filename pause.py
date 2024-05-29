fb92dfa8bc2e251f4fbd5a25924528a04bb28e5c
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-29 08:17:56 +0400

    Update pause.py

--- pause.py
+++ pause.py
@@ -9,9 +9,9 @@ from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["pause", "cpause", "durdur"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["duraklat", "cduraklat", "durdur"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
-async def pause_admin(cli, message: Message, _, chat_id):
+async def duraklat_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
@@ -19,3 +19,4 @@ async def pause_admin(cli, message: Message, _, chat_id):
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
+    
