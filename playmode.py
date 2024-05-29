325b106f15edd68da8106a92283a183ad1b431e9
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-29 17:00:23 +0400

    Update playmode.py

--- playmode.py
+++ playmode.py
@@ -8,26 +8,27 @@ from AnonXMusic.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["playmode", "mode"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["oynatmamodu", "mod"]) & filters.group & ~BANNED_USERS)
 @language
-async def playmode_(client, message: Message, _):
-    playmode = await get_playmode(message.chat.id)
-    if playmode == "Direct":
-        Direct = True
+async def oynatma_modu(client, message: Message, _):
+    oynatma_modu = await get_playmode(message.chat.id)
+    if oynatma_modu == "Doğrudan":
+        Dogrudan = True
     else:
-        Direct = None
-    is_non_admin = await is_nonadmin_chat(message.chat.id)
-    if not is_non_admin:
-        Group = True
+        Dogrudan = None
+    yonetici_degil_mi = await is_nonadmin_chat(message.chat.id)
+    if not yonetici_degil_mi:
+        Grup = True
     else:
-        Group = None
-    playty = await get_playtype(message.chat.id)
-    if playty == "Everyone":
-        Playtype = None
+        Grup = None
+    oynatma_tipi = await get_playtype(message.chat.id)
+    if oynatma_tipi == "Herkes":
+        Oynatmatipi = None
     else:
-        Playtype = True
-    buttons = playmode_users_markup(_, Direct, Group, Playtype)
-    response = await message.reply_text(
-        _["play_22"].format(message.chat.title),
-        reply_markup=InlineKeyboardMarkup(buttons),
+        Oynatmatipi = True
+    butonlar = playmode_users_markup(_, Dogrudan, Grup, Oynatmatipi)
+    yanit = await message.reply_text(
+        _["oynat_22"].format(message.chat.title),
+        reply_markup=InlineKeyboardMarkup(butonlar),
     )
+    
